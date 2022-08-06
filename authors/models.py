722e62ce6975ae5833ae8beb67bbd9d486536df3
Kasia Kurek <kasia@macbook-air-kasia.home>
2022-08-06 12:05:50 +0200

    model Autor

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Autor(models.Model):
+    imie = models.CharField(max_length=200)
+    nazwisko = models.CharField(max_length=200)
+    wiek = models.DateField(null=True, blank=True)
+
+
