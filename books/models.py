067f69b37ec5538fb10cce7c239b92c61f6c7387
Dawid Machala <iduopro@gmail.com>
2022-08-06 13:06:01 +0200

    fixtures

--- books/models.py
+++ books/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+
 class Book(models.Model):
     title = models.CharField(max_length=250)
     description = models.TextField()
     year = models.IntegerField()
+    author = models.ForeignKey(
+        "authors.Autor", on_delete=models.CASCADE, related_name="books"
+    )
